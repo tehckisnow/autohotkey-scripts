abd9de50fdeb0afce8621412bc0bf312383ebccb
tehckisnow <chetwonsik@gmail.com>
2020-01-07 18:48:22 -0500

    adding cmd script

---
+++ aliases.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+cd ..
+cd ..
+
+doskey r=C:\Users\"Delta_6"\Desktop\"Literal Garbage"\programs\vifm-w64-se-0.10-binary\vifm.exe
+doskey ls=dir
+doskey clear=cls
+doskey vlc=C:\"Program Files (x86)"\VideoLAN\VLC\vlc.exe
+doskey rm=del
+doskey manjo=C:\"Program Files"\Oracle\VirtualBox\VBoxManage startvm manjobox
+doskey sim=C:\Users\Delta_6\Desktop\"Literal Garbage"\ahkscripts\autohotkey-scripts\simple.ahk
